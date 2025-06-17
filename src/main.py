79a717f960d8bf8d434a2ac3ca0243c3b05d2ae8
Favour Adekola <100747801+FavourAdekola@users.noreply.github.com>
2025-06-16 20:06:24 -0400

    Rename Files

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 from user import User
-from skill_analysis import find_required_skills, find_gap
+from voyage import Voyage
 
 if __name__ == "__main__":
-    user = User("Favour Adekola")
-    user.set_skills_default()
-    print(find_required_skills(user.career_goal, user.dream_company))
-    print(find_gap(user.skills, find_required_skills(user.career_goal, user.dream_company)))
+    pass
 
