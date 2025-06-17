d25b86d4745b1d032573136ae3ab26d2d57b33cc
Harouna Thiam <harounathiam2005@gmail.com>
2025-06-17 13:16:38 -0400

    completed project idea suggestion mechanism

--- src/chat_integration/inference_call_tester.py
+++ src/chat_integration/inference_call_tester.py
@@ -1,17 +1,18 @@
-import chatportal
+# import chatportal
 import geminiportal
 
 # Call the OpenAI API
 
-desired_career = "UX/Frontend Developer"
-test_skillset = ["html, css, react, javascript, figma"]
+desired_career = "UX Designer"
+test_skillset = ["react, html, css, design theory, ux"]
 test_courseset = ["Intro to python", "intro to react", "frontend programming", "rocket science", "user experience and design", 
                   "fullstack development", "artificial intelligence", "intro to machine learning", "linear algebra", "multivariable calculus", "web design", 
                   "software design", "object oriented programming", "Figma essentials", "information management", "psychology of design", "color theory", "Bootstrapping with Angular"]
 user = f"I want to be a {desired_career}!"
 
 def main():
-    geminiportal.getCourses(test_skillset, test_courseset, user)
+    courses = geminiportal.getCourses(test_skillset, test_courseset, user)
+    geminiportal.getProjectCheckpoint(courses)
 
 if __name__ == "__main__":
     main()
